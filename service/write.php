f8124f7667c7be6770b367c54b1effa68981914e
zumextal <y.hashizume@aixtal.com>
2024-02-13 16:53:24 +0900

    config

--- service/write.php
+++ service/write.php
@@ -160,7 +160,7 @@ foreach ($session->trials as $trial) {
 }
 
 if ($write_pc) {
-	$filename = $filepathPrefix."paired_comparison".$filepathPostfix;
+	$filename = $filepathPrefix."result_ABX".$filepathPostfix;
 	$isFile = is_file($filename);
 	$fp = fopen($filename, 'a');
 	foreach ($pcCsvData as $row) {
